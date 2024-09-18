5556ab07a371b891b6854d185c213315f1e95198
Jair Silva <jair.monteria.401@gmail.com>
2024-09-18 17:42:14 -0500

    new branch

--- config/db_connection.php
+++ config/db_connection.php
@@ -2,5 +2,7 @@
     $connection = mysqli_connect("localhost","root","","delock");
     if (mysqli_connect_errno()) { 
         printf("", mysqli_connect_error());
+    }else{
+        echo "good";
     }
 ?>
